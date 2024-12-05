871da92688ecdda8996f41e61cfe4293ad05c836
AgrahariNisha <155371640+AgrahariNisha@users.noreply.github.com>
2024-12-05 14:54:18 +0530

    Add files via upload

---
+++ StationaryShop.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class StationaryShop 
+{
+  public static void main(String[] args) {
+    // Initialization
+    String[] products = {"Pens", "Pencils", "Notebooks", "Staplers", "Glue"};
+    double[] prices = {5, 5, 50, 100, 15};
+    int[] quantities = {100, 200, 50, 20, 30};
+    double totalSales = 0.0;
+
+    // Scanner to read user input
+    Scanner scanner = new Scanner(System.in);
+
+    // Displaying the menu
+    System.out.println("Welcome to the stationary shop!");
+    System.out.println("-------------------------------");
+    for (int i = 0; i < products.length; i++) {
+      System.out.println((i + 1) + ". " + products[i] + " - $" + prices[i]);
+    }
+    System.out.println("-------------------------------");
+
+    // Execute until quit
+    while (true) {
+      System.out.print("Enter the number of the product you'd like to purchase (or 0 to exit): ");
+      int choice = scanner.nextInt();
+
+      if (choice == 0) {
+        break;
+      } else if (choice > 0 && choice <= products.length) {
+        System.out.print("Enter the quantity you'd like to purchase: ");
+        int quantity = scanner.nextInt();
+
+        if (quantity > 0 && quantity <= quantities[choice - 1]) {
+          double subtotal = prices[choice - 1] * quantity;
+          totalSales += subtotal;
+          quantities[choice - 1] -= quantity;
+          System.out.println("You purchased " + quantity + " " + products[choice - 1] + "(s) for $" + subtotal);
+        } else {
+          System.out.println("Sorry, we don't have enough stock.");
+        }
+      } else {
+        System.out.println("Invalid choice. Please try again.");
+      }
+    }
+
+    System.out.println("Thank you for shopping at the Stationary Shop!");
+    System.out.println("Total sales: $" + totalSales);
+  }
+} 
+
+
+
